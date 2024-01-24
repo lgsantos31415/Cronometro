35c3deab29909a9745ca9fb5f560f70f004ec0df
Luiz <guga.santos0810@gmail.com>
2024-01-24 18:38:03 -0300

    update

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 let play = document.querySelector("#play")
-
+let minutes = document.querySelector("#minutes")
+let seconds = document.querySelector("#seconds")
 let isRunning = false
 
 function start(minutes, seconds){
-
     updateDisplay(minutes, seconds)
 
     play.addEventListener("click", () => {
@@ -12,32 +12,50 @@ function start(minutes, seconds){
     })
 }
 
-function updateDisplay(minutes, seconds){
-    document.querySelector("#minutes").textContent = String(minutes).padStart(2, "0")
-    document.querySelector("#seconds").textContent = String(seconds).padStart(2, "0")
+function updateDisplay(m, s){
+    minutes.textContent = String(m).padStart(2, "0")
+    seconds.textContent = String(s).padStart(2, "0")
 }
 
 function countDown(){
-    if(isRunning === false) return
+    if(isRunning == false) return
 
-    let minutes = Number(document.querySelector("#minutes").textContent)
-    let seconds = Number(document.querySelector("#seconds").textContent)
+    let m = Number(minutes.textContent)
+    let s = Number(seconds.textContent)
 
-    seconds--
+    s--
 
-    if(seconds < 0){
-        seconds = 59
-        minutes--
+    if(s < 0){
+        s = 59
+        m--
     }
 
-    if(minutes < 0){
+    if(m < 0){
         isRunning = false
         return
     }
 
-    updateDisplay(minutes, seconds)
-
+    updateDisplay(m, s)
     setTimeout(() => countDown(), 1000)
 }
 
-start(0, 5)
+minutes.addEventListener("click", e => edit(e))
+minutes.addEventListener("blur", e => blur(e))
+seconds.addEventListener("click", e => edit(e))
+seconds.addEventListener("blur", e => blur(e))
+
+minutes.onkeypress = e => /\d/.test(e.key)
+seconds.onkeypress = e => /\d/.test(e.key)
+
+function edit(e){
+    isRunning = false
+    e.target.textContent = ""
+    e.target.setAttribute("contenteditable", "true")
+    e.target.focus()
+}
+
+function blur(e){
+    if(e.target.textContent > 60) e.target.textContent = 60
+}
+
+start(1, 30)
